31c69bcbc081ca03b3f1db583e259127594f1b03
bhadresh-goswami <tops.bhadresh@gmail.com>
2019-01-05 19:13:16 +0530

    upload

--- lazyStoredProperty/main.swift
+++ lazyStoredProperty/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+
+class Class1 {
+    var arr = [Int]()
+    init() {
+        print("Class1's Object Created!")
+    }
+}
+
+class Class2 {
+    var arr2 = [String]()
+    
+    //object of class 1
+    lazy var obj1 = Class1()
+    
+    init() {
+        print("Class2's Object Created!")
+    }
+}
+
+var ObjClass2 = Class2()
+ObjClass2.arr2 = ["aaa","bbb"]
+
+ObjClass2.obj1.arr = [11,22,33]
+
+
 
